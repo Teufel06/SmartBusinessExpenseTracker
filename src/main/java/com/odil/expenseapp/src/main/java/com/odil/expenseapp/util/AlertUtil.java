598eb060e388f04cbb4491bd933cfe2c7938142e
Teufel06 <162578016+Teufel06@users.noreply.github.com>
2025-10-17 11:11:12 +0530

    Create AlertUtil.java

---
+++ src/main/java/com/odil/expenseapp/src/main/java/com/odil/expenseapp/util/AlertUtil.java
@@ -0,0 +1,14 @@
+package com.odil.expenseapp.util;
+
+import javafx.scene.control.Alert;
+
+public class AlertUtil {
+    public static void info(String title, String msg) {
+        Alert a = new Alert(Alert.AlertType.INFORMATION);
+        a.setTitle(title); a.setHeaderText(null); a.setContentText(msg); a.showAndWait();
+    }
+    public static void error(String title, String msg) {
+        Alert a = new Alert(Alert.AlertType.ERROR);
+        a.setTitle(title); a.setHeaderText(null); a.setContentText(msg); a.showAndWait();
+    }
+}
